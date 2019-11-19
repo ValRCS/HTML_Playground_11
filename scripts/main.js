f8d76474e61f123af9704d0673d90207efb4e73f
Valdis Saules <valdis.s.coding@gmail.com>
2019-11-19 13:00:21 +0200

    JS examples

--- scripts/main.js
+++ scripts/main.js
@@ -1,42 +1,84 @@
 'use strict';
-console.log("Running my JS");
-var a = 55;
-var b = 20;
-console.log(a);
 
-a = 300 + b + a;
-console.log(a);
-a++;
-console.log(a);
-a++;
-console.log(a);
-a++;
+function main() {
+    console.log("Running my JS");
+    var a = 55;
+    var b = 20;
+    console.log(a);
 
-var myStr = "Valdis said \"Hello\"";
-var mySecondStr = 'Valdis said "Hello"';
-var myThirdStr = `I can use "quotes" and also 'single quotes' and no problem`;
-var myFourthStr = `I can even include a variable ${a} in my text`;
-console.log(myStr);
-console.log(mySecondStr);
-console.log(myThirdStr);
-console.log(myFourthStr);
+    a = 300 + b + a;
+    console.log(a);
+    a++;
+    console.log(a);
+    a++;
+    console.log(a);
+    a++;
 
-var myText = "";
-myText += "First Line\n";
-//do more stuff
-myText += "Second Line <h1>My Headline</h1>";
-myText += "more text on second line";
-console.log(myText);
+    var myStr = "Valdis said \"Hello\"";
+    var mySecondStr = 'Valdis said "Hello"';
+    var myThirdStr = `I can use "quotes" and also 'single quotes' and no problem`;
+    var myFourthStr = `I can even include a variable ${a} in my text`;
+    console.log(myStr);
+    console.log(mySecondStr);
+    console.log(myThirdStr);
+    console.log(myFourthStr);
 
-var someAdjective = "fascinating";
-var myStr = "Learning to code is ";
-myStr += someAdjective;
-console.log(myStr);
+    var myText = "";
+    myText += "First Line\n";
+    //do more stuff
+    myText += "Second Line <h1>My Headline</h1>";
+    myText += "more text on second line";
+    console.log(myText);
 
-var myPar = document.querySelector('#myResults');
-myPar.innerText = "<em>" + myStr + "</em>";
+    var someAdjective = "fascinating";
+    var myStr = "Learning to code is ";
+    myStr += someAdjective;
+    console.log(myStr);
 
-var myFancyPar = document.querySelector('#myFancyResults');
-myFancyPar.innerHTML = "<em>" + myStr + "</em>";
+    var myFancyPar = document.querySelector('#myFancyResults');
+    myFancyPar.innerHTML = "<em>" + myStr + "</em>";
+
+    var myNoun = "dog";
+    var myAdjective = "big";
+    var myVerb = "ran";
+    var myAdverb = "quickly";
+
+    var wordBlanks = "My " + myAdjective + " " + myNoun +
+        " " + myVerb + " " + myAdverb + ".";
+
+    console.log(wordBlanks);
+
+    var myPar = document.querySelector('#myResults');
+    myPar.innerText = wordBlanks;
+}
+
+// function badGlobal() {
+// use 'use strict' to catch these kind of erros
+//     globalScopeVar = 55;
+// }
+
+function checkLocal() {
+    var myLocal = 55;
+    console.log(myLocal);
+}
+
+function add(a, b) {
+    let result = 0;
+    //Do more stuff with a and b here
+    result += a;
+    result += b;
+    return result;
+}
+
+// main();
+let myVar = checkLocal();
+console.log(typeof myVar);
+// console.log(myLocal);
+
+console.log(add(6, 7));
+console.log(add(-6, 7));
+console.log(add(6, "Valdis"));
+
+
 
 
