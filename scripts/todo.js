27e0d81a3795ceba53c299f5f1e04d22280c1b8d
Valdis Saules <valdis.s.coding@gmail.com>
2019-11-26 11:02:14 +0200

    Add new job in TODO

--- scripts/todo.js
+++ scripts/todo.js
@@ -1,8 +1,19 @@
 console.log("Started TODO app!");
 
-const container = document.querySelector('.container');
-console.log("container tag" + container.tagName);
-const children = container.childNodes;
-for (let child of children) {
-    console.log("My element has tag:" + child.tagName);
+const addBtn = document.getElementById('todo-add');
+
+addBtn.onclick = () => {
+    console.log("You Clicked ADD");
+    const el = document.createElement('div');
+    const jobInput = document.getElementById('todo-input');
+    el.innerText = jobInput.value;
+    document.getElementById('todo-list').appendChild(el);
+
 }
+
+// const container = document.querySelector('.container');
+// console.log("container tag" + container.tagName);
+// const children = container.childNodes;
+// for (let child of children) {
+//     console.log("My element has tag:" + child.tagName);
+// }
