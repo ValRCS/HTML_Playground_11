a614c9328f36a7815c42dddbb17e9f383d30d91a
Valdis Saules <valdis.s.coding@gmail.com>
2019-11-26 10:27:28 +0200

    handling change and input

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,13 +1,18 @@
 console.log("Starting my calc!");
 
+// const myelements = {}; for more complicated state better to store in one place
+
 const myaddbutton = document.getElementById('addbut');
+const myinp1 = document.getElementById('val1');
+const myinp2 = document.getElementById('val2');
+
+
 myaddbutton.onclick = () => {
     console.log("ADDING");
 
     //get numbers from two inputs
-    const myinp1 = document.getElementById('val1');
-    const myinp2 = document.getElementById('val2');
-    const resultElement = document.getElementById('result');
+
+
 
     console.log('Inp1 value: ' + myinp1.value);
     //below same as myinp2.value
@@ -15,11 +20,26 @@ myaddbutton.onclick = () => {
 
     //this will not quite work as we have strings as values
     console.log("My result is:" + (myinp1.value + myinp2.value));
+    updateResult();
+};
 
+function updateResult() {
+    const resultElement = document.getElementById('result');
     const val1 = parseFloat(myinp1.value);
     const val2 = parseFloat(myinp2.value);
 
     console.log("Real result is this:" + (val1 + val2));
     //show result in resul div
     resultElement.innerText = val1 + val2;
+}
+
+
+myinp1.oninput = (event) => {
+    console.log("oninput fired! New value is:" + event.target.value);
+    updateResult();
+};
+
+//on change fires less often than oninput , only when value is finalized(as by pressing enter)
+myinp1.onchange = (event) => {
+    console.log("onchange fired! New value is:" + event.target.value);
 };
