88962a458c1bb1d97c06f8b694c9b9202cc97e1a
Valdis Saules <valdis.s.coding@gmail.com>
2019-11-29 10:51:32 +0200

    Added Vue examples

--- scripts/myvue.js
+++ scripts/myvue.js
@@ -1,12 +1,48 @@
 console.log("Running my own Vue!");
 
 
-let app = new Vue({
+const app = new Vue({
     el: '#app',
     data: {
-        message: "Hello RCS!",
-        secondmessage: "Hello again"
+        message: 'You loaded this page on ' + new Date().toLocaleString(),
+        secondmessage: "Hello again",
+        second: "mysecond",
+        seen: true,
+        seenMsg: "Turn me OFF!",
+        todos: [
+            { text: 'Learn JavaScript' },
+            { text: 'Learn Vue' },
+            { text: 'Build something awesome' }
+        ]
+    },
+    methods: {
+        reverseMessage: function () {
+            this.message = this.message.split('').reverse().join('');
+            this.secondmessage = this.secondmessage.toUpperCase();
+        },
+        toggleSeen: function () {
+            this.seen = !this.seen;
+            if (this.seen) {
+                this.seenMsg = "Turn me OFF!";
+            } else {
+                this.seenMsg = "Turn me ON!";
+            }
+        }
     }
+});
+
+const app2 = new Vue({
+    el: '#app2',
+    data: {
+        todos: [
+            { text: 'Learn JavaScript' },
+            { text: 'Learn Vue' },
+            { text: 'Build something awesome' }
+        ]
+    }
+});
 
 
-})
+const myval = 42;
+const mystring = `My value is ${myval}`;
+console.log(mystring);
